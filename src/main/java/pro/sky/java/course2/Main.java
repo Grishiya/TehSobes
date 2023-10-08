082172dab6d497d85f55c1377e96d599276a5781
Grishiya <guruwolf8@gmail.com>
2023-10-08 08:10:21 +0300

    Задача с обработкой двух исключений

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -1,18 +1,17 @@
 package pro.sky.java.course2;
 
 
-
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Grisha", 16);
         try {
-
+            System.out.println(student);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
         } catch (RuntimeException e) {
             System.out.println(e);
-        }finally {
-            System.out.println("Все отличыно "+ student);
+        } finally {
+            System.out.println("Все отлично ");
         }
     }
 
