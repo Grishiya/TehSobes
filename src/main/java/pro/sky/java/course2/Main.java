6dafdcd7162d6146cb6a3ddaa3fcb664c25ee07c
Grishiya <guruwolf8@gmail.com>
2023-10-08 08:01:11 +0300

    Задача с обработкой двух исключений

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -1,7 +1,19 @@
 package pro.sky.java.course2;
 
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student = new Student("Grisha", 16);
+        try {
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }finally {
+            System.out.println("Все отличыно "+ student);
+        }
     }
+
 }
--- src/main/java/pro/sky/java/course2/Student.java
+++ src/main/java/pro/sky/java/course2/Student.java
@@ -1,2 +1,23 @@
-package pro.sky.java.course2;public class Student {
+package pro.sky.java.course2;
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        if (name == null) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        this.name = name;
+        if (age < 7) {
+            throw new RuntimeException("Тебе рано в школу");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name " + this.name
+                + " , Age " + this.age;
+    }
 }
